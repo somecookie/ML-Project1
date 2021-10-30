5fcdd16aca1566c626b70f14719cd19c07a2ad99
somecookie <ferreiraribeiro.ricardo@gmail.com>
2021-10-30 12:25:08 +0200

    add Ridge regression

--- scripts/implementations.py
+++ scripts/implementations.py
@@ -2,7 +2,51 @@ import numpy as np
 import matplotlib as plt
 import threading
 
-def split_data_jet_num(x, y, headers):
+############### utils function
+
+def process_data(x):
+    """
+    process_data processes the data as follows:
+    - split the dataset in 3 datasets according to the jet number feature (for value 0, 1 and 2/3)
+    - remove unnecessary features, i.e., highly correlated features, phi-features and undefined features with the jet number
+    - set the remaining undefined values to the median of the feature
+    - standardize the data
+
+    Parameters:
+        x (numpy.array): NxD matrix containing the dataset
+
+    Returns:
+        xs, idx_split (tuple(list(np.array))): the lists contain the processed data for each category and their respective index.
+    """
+
+    # indexes that we want to keep after the processing for each category
+    idx_remaining_features = [
+        [0,1,2,3,7,8,9,11,13,14,16,17,19,21],
+        [0,1,2,3,7,8,9,11,13,14,16,17,19,21,23,24,29],
+        [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,19,21,23,24,26,27,28,29]
+    ]
+
+    idx_split = [
+        np.argwhere(x[:,22]==0),
+        np.argwhere(x[:,22]==1),
+        np.argwhere(x[:,22]>1)
+    ]
+
+    xs = []
+
+    for i in range(len(idx_remaining_features)):
+        x_temp = x[idx_split[i], idx_remaining_features[i]]
+        x_temp = set_undefined_to_median(x_temp)
+        x_temp,_,_ = standardize(x_temp)
+        xs.append(x_temp)
+
+    return xs, idx_split
+
+    
+
+
+
+def split_data_jet_num(x, y):
     """
     split_data_jet_num splits the data in 3 according to the jet number value (0, 1 and 2-3).
     
@@ -15,44 +59,19 @@ def split_data_jet_num(x, y, headers):
         x0, x1, x23 (tuple((numpy.array))): 3 datasets that for which the value PRI_jet_num is 0, 1 or 2-3
     """
 
-    jet_num_idx = headers.index('PRI_jet_num')
+    jet_num_idx = 22
     x0 = x[x[:, jet_num_idx] == 0, :]
-    x0 = np.concatenate((x0[:, :jet_num_idx], x0[:, jet_num_idx+1:]), axis=1)
     y0 = y[x[:, jet_num_idx] == 0]
     x1 = x[x[:, jet_num_idx] == 1, :]
-    x1 = np.concatenate((x1[:, :jet_num_idx], x1[:, jet_num_idx+1:]), axis=1)
     y1 = y[x[:, jet_num_idx] == 1]
     x2 = x[x[:, jet_num_idx] == 2, :]
     y2 = y[x[:, jet_num_idx] == 2]
     x3 = x[x[:, jet_num_idx] == 3, :]
     y3 = y[x[:, jet_num_idx] == 3]
     x23 = np.concatenate((x2,x3))
-    x23 = np.concatenate((x23[:, :jet_num_idx], x23[:, jet_num_idx+1:]), axis=1)
     y23 = np.concatenate((y2,y3))
 
-    return x0, y0, x1, y1, x23,y23
-
-def remove_undefined_features(x, headers, print_undefined=False):
-    """
-    remove_undefined_features removes the undefined features, i.e., the features for which all entries are -999.
-
-    Parameters:
-
-        x (numpy.array)         : NxD matrix containing the dataset
-        headers (list(str))     : list of length D containing the names of the features
-        print_undefined (bool)  : print the undefined headers in alphabetical order
-    
-    Returns:
-        numpy.array: the dataset without the undefined features
-    """
-
-    defined_idx = np.mean(x, axis=0) > -999
-
-    if print_undefined:
-        undefined_headers = "\n".join(sorted(["- " + headers[i] for i in np.arange(x.shape[1])[~defined_idx]]))
-        print(f"Undefined headers:\n{undefined_headers}")
-    
-    return x[:, defined_idx]
+    return [x0, x1, x23],[y0, y1, y23]
 
 def set_undefined_to_median(x):
     """
@@ -63,45 +82,6 @@ def set_undefined_to_median(x):
         x[x[:,i] == -999, i] = medians[i]
     
     return x
-
-def remove_phi_features(x, headers):
-    """
-    remove_phi_features removes the features ending with the suffix _phi
-    """
-    idx = [i for i in range(len(headers)) if headers[i].endswith('_phi')]
-    new_size = len(idx)
-    new_x = np.zeros((x.shape[0], x.shape[1]-new_size))
-    new_headers = []
-    j = 0
-
-    for i in range(len(headers)):
-        if i in idx:
-            continue
-
-        new_headers.append(headers[i])
-        new_x[:, j] = x[:, i]
-        j+=1
-
-    return new_x, new_headers
-
-def find_correlated_features(x, threshold=0.7):
-    """
-    find_correlated_features finds all pairs of features which have a correlation higher than the threshold.
-    """
-
-    correlated_features = {}
-
-    for i in range(x.shape[1]-1):
-        for j in range(i+1, x.shape[1]):
-            xi = x[:, i]
-            xj = x[:, j]
-
-            corr = np.corrcoef(xi, xj)[0,1]
-
-            if corr > threshold:
-                correlated_features[i] = j
-
-    return correlated_features
     
 def standardize(x):
     """Standardize the original data set."""
@@ -117,7 +97,7 @@ def compute_mse(y, tx, w):
     """
     e = y - np.matmul(tx, w)
     N = len(y)
-    return 1/(2*N)*np.dot(e, e)
+    return 1/(2*N)*e.T@e
 
 def compute_rmse(y, tx, w):
     """Calculate the rmse loss.
@@ -156,112 +136,6 @@ def build_poly(x, degree):
         poly = np.c_[poly, x**i]
     
     return poly
-
-class ThreeModels:
-    def __init__(self, x0,y0,x1,y1,x23,y23):
-        self.models = [RidgeRegressionModel(x0, y0, "model0"), RidgeRegressionModel(x1, y1, "model1"), RidgeRegressionModel(x23, y23, "model23")]
-    
-    def train(self, degrees, lambdas, k_folds):
-        threads = []
-
-        # traine each model in its own threas
-        for model in self.models:
-            t = threading.Thread(target=model.CV_ridge_regression, args=(degrees, lambdas, k_folds))
-            threads.append(t)
-            t.start()
-
-        # wait for each thread to finish
-        for t in threads:
-            t.join()
-        
-        print("Finish training")
-
-class RidgeRegressionModel:
-    def __init__(self,x,y,name):
-        self.x = x
-        self.y = y
-        self.rmse_te = None
-        self.accuracy = None
-        self.rmse_tr = None
-        self.best_deg = None
-        self.best_lambda = None
-        self.name = name
-
-
-    def CV_ridge_regression(self,degrees, lambdas, k_folds, print_res=False):
-        #prepare cross validation
-        k_indices = build_k_indices(self.y, k_folds)
-
-        #store loss in matrices
-        rmse_tr = np.empty((len(degrees), len(lambdas)))
-        rmse_te = np.empty((len(degrees), len(lambdas)))
-        accuracy = np.empty((len(degrees), len(lambdas)))
-
-        for idx_deg, d in enumerate(degrees):
-            for idx_lambda, l in enumerate(lambdas):
-                rmse_tr_tmp = []
-                rmse_te_tmp = []
-                accuracy_tmp = []
-                for k in range(k_folds):
-                    
-                    # get the k-fold
-                    x_tr, y_tr, x_te, y_te = split_data_from_k_indices(self.y, self.x, k_indices, k)
-    
-                    # build polynomial extension
-                    x_te_poly = build_poly(x_te, d)
-                    x_tr_poly = build_poly(x_tr, d)
-                    
-                    # train
-                    _, w = ridge_regression(y_tr, x_tr_poly, l)
-                    
-                    # compute loss for training, loss and accuarcy for testing
-                    rmse_tr_tmp.append(compute_rmse(y_tr, x_tr_poly, w))
-                    rmse_te_tmp.append(compute_rmse(y_te, x_te_poly, w))
-                    accuracy_tmp.append(get_accuracy(y_te, x_te_poly, w))
-                
-                rmse_te_mean = np.mean(rmse_te_tmp)
-                rmse_tr_mean = np.mean(rmse_tr_tmp)
-                accuracy_mean = np.mean(accuracy_tmp)
-                
-                if print_res:
-                    print("D: {:2d} L: {:1.10f} RMSE TR: {:.5f} RMSE TE: {:.5f} ACC: {:1.5f}".format(d, l, rmse_tr_mean, rmse_te_mean,accuracy_mean))
-                
-                rmse_te[idx_deg, idx_lambda] = rmse_te_mean
-                rmse_tr[idx_deg, idx_lambda] = rmse_tr_mean
-                accuracy[idx_deg, idx_lambda] = accuracy_mean
-
-        max_acc_idx = np.where(accuracy == np.max(accuracy))
-        
-        self.best_deg = degrees[max_acc_idx[0]][0]
-        self.best_lambda = lambdas[max_acc_idx[1]][0]
-
-        print("{:7s} Degree: {:2d} Lambda: {:.10f} Accuracy: {:.3f}, ".format(self.name,self.best_deg, self.best_lambda, np.max(accuracy)))
-
-        self.rmse_te = rmse_te
-        self.accuracy = accuracy
-        self.rmse_tr = rmse_tr
-
-
-def heatmap_accuracy(accuracy, lambdas, degrees, figsize):
-    fig, ax = plt.subplots(figsize=figsize)
-    im = ax.imshow(accuracy.T)
-
-
-    # We want to show all ticks...
-    ax.set_yticks(np.arange(len(lambdas)))
-    ax.set_xticks(np.arange(len(degrees)))
-    # ... and label them with the respective list entries
-    ax.set_yticklabels(lambdas)
-    ax.set_xticklabels(degrees)
-    ax.set_ylabel("Lambda")
-    ax.set_xlabel("Degree")
-
-    fig.colorbar(im, ax=ax)
-
-    ax.set_title("Accuracy for the ridge regression CV on the degree and lambda")
-
-    fig.tight_layout()
-    plt.show()
 
 def split_data(x, y, ratio, seed=1):
     """
@@ -307,34 +181,165 @@ def split_data_from_k_indices(y, x, k_indices, k):
 
     return x_tr, y_tr, x_te, y_te
 
-def get_rates(y, pred):
-    # predict 1 and label is 1
-    true_pos = np.sum(np.logical_and(pred == 1, y == 1))
-    # predict -1 and label is -1
-    true_neg = np.sum(np.logical_and(pred == -1, y == -1))
-    # predict 1 and label is -1
-    false_pos = np.sum(np.logical_and(pred == 1, y == -1))
-    # predict -1 and label is 1
-    false_neg = np.sum(np.logical_and(pred == -1, y == 1))
+############### Class for our model ###############
+class ThreeModels:
+    """
+    ThreeModels is the class for our model. It is composed of 3 underlying simpler (Ridge Regression) models.
+    """
+    def __init__(self, xs, ys):
 
-    return true_pos, true_neg, false_pos, false_neg
+        assert len(xs) == len(ys)
 
-def get_accuracy_from_rates(true_pos, true_neg, false_pos, false_neg):
-    return (true_pos + true_neg)/(true_pos + true_neg + false_neg + false_pos)
+        self.models = [RidgeRegressionModel(xs[i], ys[i], f"model{i}") for i in range(len(xs))]
+    
+    def train(self, degrees, lambdas, k_folds):
+        """
+        train trains the 3 underlying models using CV.
+        """
+        threads = []
 
-def get_f1score(true_pos, false_pos, false_neg):
-    return true_pos/(true_pos + 0.5*(false_pos + false_neg))
+        # traine each model in its own threas
+        for model in self.models:
+            t = threading.Thread(target=model.CV_ridge_regression, args=(degrees, lambdas, k_folds))
+            threads.append(t)
+            print(f"Start training for model {model.name}")
+            t.start()
 
-def show_confusion_matrix(true_pos, true_neg, false_pos, false_neg):
-    confusion_matrix = np.array([[true_pos, false_pos], [false_neg, true_neg]])
+        # wait for each thread to finish
+        for t in threads:
+            t.join()
+        
+        print(50*"=")
+        print("Finish training")
+        print(50*"=")
+    
+    def save_CV_results(self, path):
+        """
+        save_CV_results saves the results in a binary file at path containing (in this order) the np.arrays accuracy, rmse of training and rmse of testing
+        """
+        for model in self.models:
+            assert model.rmse_tr is not None and model.rmse_te is not None and model.accuracy is not None
+        
+        for model in self.models:
+            with open(f"{path}_{model.name}", "wb") as f:
+                np.save(f, model.accuracy)
+                np.save(f, model.rmse_tr)
+                np.save(f, model.rmse_te)
+    
+    def load_CV_results(self, path):
+        """
+        load_CV_results loads the results from a file at path and populate the underlying np.array for accuracy, rmse of training and rmse of testing
+        """
+        for model in self.models:
+            assert model.rmse_tr is None and model.rmse_te is None and model.accuracy is None
+        
+        for model in self.models:
+            with open(f"{path}_{model.name}", "rb") as f:
+                model.accuracy = np.load(f)
+                model.rmse_tr = np.load(f)
+                model.rmse_te = np.load(f)
+    
+    def compute_ws(self):
+        for model in self.models:
+            assert model.best_deg is not None and model.best_lambda is not None
+        
+        for model in self.models:
+            x_poly = build_poly(model.x, model.best_deg)
+            _, w = ridge_regression(model.y, x_poly, model.best_lambda)
+            model.w = w
 
-    _, ax= plt.subplots(figsize=(7.5, 7.5))
-    ax.matshow(confusion_matrix, cmap=plt.cm.Blues, alpha=0.3)
-    for i in range(confusion_matrix.shape[0]):
-        for j in range(confusion_matrix.shape[1]):
-            ax.text(x=j, y=i,s=confusion_matrix[i, j], va='center', ha='center', size='xx-large')
+        
 
-    plt.xlabel('Predictions', fontsize=18)
-    plt.ylabel('Actuals', fontsize=18)
-    plt.title('Confusion Matrix', fontsize=18)
-    plt.show()
+
+class RidgeRegressionModel:
+    """
+    RidgeRegressionModel is the class that contains the code for ridge regression
+    """
+    def __init__(self,x,y,name):
+        self.x = x
+        self.y = y
+        self.rmse_te = None
+        self.accuracy = None
+        self.rmse_tr = None
+        self.best_deg = None
+        self.best_lambda = None
+        self.name = name
+        self.w = None
+
+
+    def CV_ridge_regression(self,degrees, lambdas, k_folds):
+        """
+        CV_ridge_regression performs CV grid search for the hyper parameters degree and lambda
+        """
+        #prepare cross validation
+        k_indices = build_k_indices(self.y, k_folds)
+
+        #store loss in matrices
+        rmse_tr = np.empty((len(degrees), len(lambdas)))
+        rmse_te = np.empty((len(degrees), len(lambdas)))
+        accuracy = np.empty((len(degrees), len(lambdas)))
+
+        best_acc = 0
+        best_deg = 0
+        best_l = 0
+        total_iter = len(degrees)*len(lambdas)
+        it = 0
+        steps = [0.1*i for i in range(1,10)]
+
+        for idx_deg, d in enumerate(degrees):
+            for idx_lambda, l in enumerate(lambdas):
+                
+                # print each 10%
+
+                if len(steps) > 0 and it/total_iter > steps[0]:
+                    steps = steps[1:]
+                    print(50*"=")
+                    print(self.name)
+                    print("{:.2f}% of training done".format(100*it/total_iter))
+                    print("Intermediate result:")
+                    print("Degree: {:2d} Lambda: {:.10f} Accuracy: {:.3f}".format(best_deg, best_l, best_acc))
+                    
+                it += 1
+
+                rmse_tr_tmp = []
+                rmse_te_tmp = []
+                accuracy_tmp = []
+                for k in range(k_folds):
+                    
+                    # get the k-fold
+                    x_tr, y_tr, x_te, y_te = split_data_from_k_indices(self.y, self.x, k_indices, k)
+    
+                    # build polynomial extension
+                    x_te_poly = build_poly(x_te, d)
+                    x_tr_poly = build_poly(x_tr, d)
+                    
+                    # train
+                    _, w = ridge_regression(y_tr, x_tr_poly, l)
+                    
+                    # compute loss for training, loss and accuarcy for testing
+                    rmse_tr_tmp.append(compute_rmse(y_tr, x_tr_poly, w))
+                    rmse_te_tmp.append(compute_rmse(y_te, x_te_poly, w))
+                    accuracy_tmp.append(get_accuracy(y_te, x_te_poly, w))
+                
+                rmse_te[idx_deg, idx_lambda] = np.mean(rmse_te_tmp)
+                rmse_tr[idx_deg, idx_lambda] = np.mean(rmse_tr_tmp)
+                mean_acc = np.mean(accuracy_tmp)
+                accuracy[idx_deg, idx_lambda] = mean_acc
+
+                if mean_acc > best_acc:
+                    best_acc = mean_acc
+                    best_deg = d
+                    best_l = l
+
+        
+        self.best_deg = best_deg
+        self.best_lambda = best_l
+
+        print(50*"=")
+        print(self.name)
+        print("Final result:")
+        print("Degree: {:2d} Lambda: {:.10f} Accuracy: {:.3f}".format(best_deg, best_l, best_acc))
+
+        self.rmse_te = rmse_te
+        self.accuracy = accuracy
+        self.rmse_tr = rmse_tr
